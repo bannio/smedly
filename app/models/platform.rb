82029d92d3bc880e49b0ed0e3c7fafd8143fae3a
Owen Bannister <owen@bandadbro.com>
2015-03-16 19:24:25 +0000

    adds default scope to platform and word count to posts

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,4 +1,6 @@
 class Platform < ActiveRecord::Base
 
   has_many :posts
+
+  default_scope { order(:name) }
 end
--- spec/models/platform_spec.rb
+++ spec/models/platform_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Platform, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "default scope alphabetical order" do
+    FactoryGirl.create(:platform, name: "B")
+    FactoryGirl.create(:platform, name: "C")
+    FactoryGirl.create(:platform, name: "A")
+
+    expect(Platform.all.map(&:name)).to eq(%w{A B C})
+    
+  end
 end
