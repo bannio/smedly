8b89f035ca67758f1baadcd28ae1ba6bb46a9f6d
Owen Bannister <owen@bandadbro.com>
2015-04-02 14:32:29 +0100

    update twitter initializer

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,7 @@
-twitter = Twitter::REST::Client.new do |config|
+$twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TW_CONSUMER_KEY']
   config.consumer_secret = ENV['TW_CONSUMER_SECRET']
+  config.bearer_token = ENV['TW_BEARER_TOKEN']
   # For application only authentication we do not need the next two:
   # config.access_token = ENV['TW_ACCESS_TOKEN']
   # config.access_token_secret = ENV['TW_ACCESS_SECRET']
