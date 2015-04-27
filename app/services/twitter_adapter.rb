751c3db9e56b3bac6f0124e59175ec2e6577743e
Owen Bannister <owen@bandadbro.com>
2015-04-27 13:48:22 +0100

    refactor away twitter adapter to remove job errors

--- app/services/twitter_adapter.rb
+++ app/services/twitter_adapter.rb
@@ -1,11 +1,13 @@
 class TwitterAdapter
 
+  attr_reader :handle
+
   def initialize(handle)
     @handle = handle
   end
 
   def client
-    @client ||= Twitter::REST::Client.new(
+    client = Twitter::REST::Client.new(
     consumer_key: ENV['TW_CONSUMER_KEY'],
     consumer_secret: ENV['TW_CONSUMER_SECRET'],
     bearer_token: ENV['TW_BEARER_TOKEN'])
@@ -13,30 +15,30 @@ class TwitterAdapter
 
   def user
     # rescue Twitter::Error::NotFound
-    @user ||= client.user(@handle.name)
+    user ||= client.user(handle.name)
   end
 
-  def profile_image_url
-    user.profile_image_uri.to_s
-  end
+  # def profile_image_url
+  #   user.profile_image_uri.to_s
+  # end
 
-  def location
-    user.location
-  end
+  # def location
+  #   user.location
+  # end
 
-  def followers
-    user.followers_count
-  end
+  # def followers
+  #   user.followers_count
+  # end
 
-  def profile_description
-    user.description
-  end
+  # def profile_description
+  #   user.description
+  # end
 
-  def tweets_count
-    user.tweets_count
-  end
+  # def tweets_count
+  #   user.tweets_count
+  # end
 
-  def last_tweet_date
-    user.status.created_at
-  end
+  # def last_tweet_date
+  #   user.status.created_at
+  # end
 end
